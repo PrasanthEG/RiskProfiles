8ebbe508d940f05a0caed1284da9553ed1733768
PrasanthEG <eg.prasanth@gmail.com>
2025-03-25 09:18:47 +0530

    # Please enter the commit message for your changes. Lines starting

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY='PragmaticSecret@2024'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Pragmatic2024@risk-database.cvsou8myklaa.ap-south-1.rds.amazonaws.com:5432/risk_profile"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres123@localhost:5432/risk_profile"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESET_PASS_LINK="http://risk.pragmadigital.in/reset-password?token="
    
 
